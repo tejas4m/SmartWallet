dd4a7acb126aa91609bc4fdf2aa494326d55d86b
tejas4m <tejas4202@gmail.com>
2021-07-09 03:09:12 +0530

    env

--- smartwallet/env.bat
+++ smartwallet/env.bat
@@ -2,7 +2,6 @@ set DB_NAME=incomeexpensesdb
 set DB_USER=postgres
 set DB_USER_PASSWORD=@Sys123456
 set DB_HOST=localhost
-set EMAIL_HOST_PASSWORD = @Sys123456
-set EMAIL_HOST_USER = tejasmahajan4202@gmail.com
+se
 set EMAIL_HOST = smtp.gmail.com
 set DEFAULT_FROM_EMAIL = tejasmahajan4202@gmail.com
