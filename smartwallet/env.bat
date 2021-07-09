0c80782446a1aa5ebbeed4b375e7e3712b184ada
tejas4m <tejas4202@gmail.com>
2021-07-10 02:13:26 +0530

    adding currency.json and working with it

--- smartwallet/env.bat
+++ smartwallet/env.bat
@@ -2,6 +2,6 @@ set DB_NAME=incomeexpensesdb
 set DB_USER=postgres
 set DB_USER_PASSWORD=@Sys123456
 set DB_HOST=localhost
-se
+
 set EMAIL_HOST = smtp.gmail.com
 set DEFAULT_FROM_EMAIL = tejasmahajan4202@gmail.com
--- smartwallet/smartwallet/urls.py
+++ smartwallet/smartwallet/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('', include('expenses.urls') ),
     path('authentication/', include('authentication.urls') ),
     path('admin/', admin.site.urls),
-
+    path('preferences/', include('userpreferences.urls') ),
+    
 
 ]
